80ff0dc27d951a684d7bdb692d2fb443b933e232
kartikeya choudhary <honeychoudhary9797@gmail.com>
2024-08-24 20:39:44 +0530

    Final Changes

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const btn=document.querySelector('.button')
 const time=document.querySelector('#time')
 const date=document.querySelector('#date')
 const finaldata=document.querySelector('.finaldata')
+const selecttype=document.querySelector('#selectType');
 function updateTime(){
     let dateist=new Date()
     let datepkt=new Date()
@@ -28,18 +29,49 @@ setInterval(updateTime,1000)
 btn.addEventListener('click',checkfields)
 
 function checkfields(){
-const completestring=date.value+' '+time.value+':00'
-const dateval=new Date(completestring)
+let timeval=time.value;
+let dateval=String(date.value+" "+new Date().toLocaleTimeString());
+let currentTime=new Date(dateval)
+currentTime.setHours(currentTime.getHours()+timeval.split(':').map(e=>Number(e))[0])
+currentTime.setMinutes(currentTime.getMinutes()+timeval.split(':').map(e=>Number(e))[1])
 if (time.value==='' || date.value===''){
     alert('Please Chose the values')
 }
 else{
-    let days=String(dateval.getDate()+2).padStart(2,'0')
-    let month=String(dateval.getMonth()).padStart(2,'0')
-    let year=String(dateval.getFullYear()).slice(2)
-    let finaldate=`${days}/${month}/${year}`
-    finaldata.textContent=`War will end at ${ist.innerHTML} ist and ${pkt.innerHTML} pkt on ${finaldate}`
+    if(selecttype.value==='warstart'){
+    finaldata.textContent=`War will end at ${currentTime.toLocaleString('en-GB',{hour: 'numeric',
+    minute: 'numeric',
+    hour12:true,
+    timeZone:'Asia/Kolkata'})} ist and ${currentTime.toLocaleString('en-GB',{hour: 'numeric',
+    minute: 'numeric',
+    hour12:true,
+    timeZone:'Asia/Karachi'})} pkt on ${currentTime.toLocaleDateString()}`}
+    else{
+        let currenttime2=currentTime
+        currenttime2.setDate(currentTime.getDate()+1)
+        finaldata.textContent=`War will end at ${currentTime.toLocaleString('en-GB',{hour: 'numeric',
+            minute: 'numeric',
+            hour12:true,
+            timeZone:'Asia/Kolkata'})} ist and ${currentTime.toLocaleString('en-GB',{hour: 'numeric',
+            minute: 'numeric',
+            hour12:true,
+            timeZone:'Asia/Karachi'})} pkt on ${currenttime2.toLocaleDateString()}`
+        
+    }
 }
 }
 const copy=document.getElementById('copy')
 finaldata.addEventListener('click',()=>{navigator.clipboard.writeText(finaldata.innerHTML);copy.style.display='block';finaldata.style.color='#4bb543';finaldata.style.cursor='default'})
+flatpickr('#time',{
+    enableTime: true,
+    noCalendar: true,
+    dateFormat: "H:i",
+    time_24hr: true,
+    maxTime:'23:59'
+})
+flatpickr('#date',{
+    
+    dateFormat: "Y-m-d",
+    maxDate: new Date().fp_incr(7),
+    
+})
