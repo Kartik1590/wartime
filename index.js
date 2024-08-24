b9bc4cad811757aea5cc5629662a787982dc5c2a
kartikeya choudhary <honeychoudhary9797@gmail.com>
2024-08-24 21:42:50 +0530

    Ist time updated

--- index.js
+++ index.js
@@ -5,18 +5,22 @@ const time=document.querySelector('#time')
 const date=document.querySelector('#date')
 const finaldata=document.querySelector('.finaldata')
 const selecttype=document.querySelector('#selectType');
+const copy=document.getElementById('copy')
 function updateTime(){
     let dateist=new Date()
     let datepkt=new Date()
     let optionspkt={hour: 'numeric',
         minute: 'numeric',
         second: 'numeric',
-    hour12:true,
-timeZone:'Asia/Karachi'}
+        hour12:true,
+        timeZone:'Asia/Karachi'
+    }
 let optionsist={hour: 'numeric',
     minute: 'numeric',
     second: 'numeric',
-hour12:true}
+    hour12:true,
+    timeZone:'Asia/Kolkata'
+}
     let currentsecondist=dateist.toLocaleString('en-GB',optionsist)
     let currentsecondpkt=datepkt.toLocaleString('en-GB',optionspkt)
     ist.textContent=currentsecondist
@@ -26,42 +30,6 @@ hour12:true}
 
 setInterval(updateTime,1000)
 
-btn.addEventListener('click',checkfields)
-
-function checkfields(){
-let timeval=time.value;
-let dateval=String(date.value+" "+new Date().toLocaleTimeString());
-let currentTime=new Date(dateval)
-currentTime.setHours(currentTime.getHours()+timeval.split(':').map(e=>Number(e))[0])
-currentTime.setMinutes(currentTime.getMinutes()+timeval.split(':').map(e=>Number(e))[1])
-if (time.value==='' || date.value===''){
-    alert('Please Chose the values')
-}
-else{
-    if(selecttype.value==='warstart'){
-    finaldata.textContent=`War will end at ${currentTime.toLocaleString('en-GB',{hour: 'numeric',
-    minute: 'numeric',
-    hour12:true,
-    timeZone:'Asia/Kolkata'})} ist and ${currentTime.toLocaleString('en-GB',{hour: 'numeric',
-    minute: 'numeric',
-    hour12:true,
-    timeZone:'Asia/Karachi'})} pkt on ${currentTime.toLocaleDateString()}`}
-    else{
-        let currenttime2=currentTime
-        currenttime2.setDate(currentTime.getDate()+1)
-        finaldata.textContent=`War will end at ${currentTime.toLocaleString('en-GB',{hour: 'numeric',
-            minute: 'numeric',
-            hour12:true,
-            timeZone:'Asia/Kolkata'})} ist and ${currentTime.toLocaleString('en-GB',{hour: 'numeric',
-            minute: 'numeric',
-            hour12:true,
-            timeZone:'Asia/Karachi'})} pkt on ${currenttime2.toLocaleDateString()}`
-        
-    }
-}
-}
-const copy=document.getElementById('copy')
-finaldata.addEventListener('click',()=>{navigator.clipboard.writeText(finaldata.innerHTML);copy.style.display='block';finaldata.style.color='#4bb543';finaldata.style.cursor='default'})
 flatpickr('#time',{
     enableTime: true,
     noCalendar: true,
@@ -75,3 +43,42 @@ flatpickr('#date',{
     maxDate: new Date().fp_incr(7),
     
 })
+
+function checkfields(){
+    let timeval=time.value;
+    let dateval=String(date.value+" "+new Date().toLocaleTimeString());
+    let currentTime=new Date(dateval)
+    currentTime.setHours(currentTime.getHours()+timeval.split(':').map(e=>Number(e))[0])
+    currentTime.setMinutes(currentTime.getMinutes()+timeval.split(':').map(e=>Number(e))[1])
+    if (time.value==='' || date.value===''){
+        alert('Please Chose the values')
+    }
+    else{
+        if(selecttype.value==='warstart'){
+        finaldata.textContent=`War will end at ${currentTime.toLocaleString('en-GB',{hour: 'numeric',
+        minute: 'numeric',
+        hour12:true,
+        timeZone:'Asia/Kolkata'})} ist and ${currentTime.toLocaleString('en-GB',{hour: 'numeric',
+        minute: 'numeric',
+        hour12:true,
+        timeZone:'Asia/Karachi'})} pkt on ${currentTime.toLocaleDateString()}`}
+        else{
+            let currenttime2=currentTime
+            currenttime2.setDate(currentTime.getDate()+1)
+            finaldata.textContent=`War will end at ${currentTime.toLocaleString('en-GB',{hour: 'numeric',
+                minute: 'numeric',
+                hour12:true,
+                timeZone:'Asia/Kolkata'})} ist and ${currentTime.toLocaleString('en-GB',{hour: 'numeric',
+                minute: 'numeric',
+                hour12:true,
+                timeZone:'Asia/Karachi'})} pkt on ${currenttime2.toLocaleDateString()}`
+            
+        }
+    }
+    }
+
+btn.addEventListener('click',checkfields)
+
+
+
+finaldata.addEventListener('click',()=>{navigator.clipboard.writeText(finaldata.innerHTML);copy.style.display='block';finaldata.style.color='#4bb543';finaldata.style.cursor='default'})
